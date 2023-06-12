3e637492a63ff03f56d0bab602cdf5e786b0ea62
muhammadali246397 <muhammadali246397@gmail.com>
2023-06-12 12:20:41 +0600

    complete delete class

--- src/Pages/Dashboard/DashLayout/DashLayout.jsx
+++ src/Pages/Dashboard/DashLayout/DashLayout.jsx
@@ -8,7 +8,7 @@ const DashLayout = () => {
         <div>
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">
+                <div className="drawer-content w-4/5 mx-auto">
                     <Outlet></Outlet>
                     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
--- src/Pages/Dashboard/StudantDash/SelectedClass/SelectedClasses.jsx
+++ src/Pages/Dashboard/StudantDash/SelectedClass/SelectedClasses.jsx
@@ -1,9 +1,112 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../../AuthProvider/AuthProvider';
+import Title from '../../../Share/ReuseTitle/Title'
+import { FaTrashAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const SelectedClasses = () => {
+    const { user } = useContext(AuthContext)
+
+    const { data: addClass = [], refetch } = useQuery({
+        queryKey: ['addClass', user?.email],
+        queryFn: async () => {
+            const data = await fetch(`http://localhost:5000/addcls?email=${user?.email}`)
+            return data.json()
+        }
+    })
+
+    const deleteClass = (id) => {
+        console.log(id)
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/addclass/${id}`,{
+                    method:'DELETE'
+                })
+                .then(res => res.json())
+                .then(data => {
+                 if(data.deletedCount > 0){
+                    console.log(data)
+                    refetch()
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                      )
+                 }
+                })
+            }
+          })
+    }
+
     return (
-        <div>
-            this is selected class page
+        <div className='my-14'>
+            <Title
+                subTitle={'----- Class List -----'}
+                title={'My Selected Classes'}
+            ></Title>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    #
+                                </label>
+                            </th>
+                            <th>Image</th>
+                            <th>name</th>
+                            <th>Instructor</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            addClass.map((clas,index) => <tr key={clas._id}>
+                                <th>
+                                    <label>
+                                    {index + 1}
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={clas.classImg} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {clas.className}
+                                </td>
+                                <td>
+                                    {clas.instructorName}
+                                </td>
+                                <td className='text-orange-600'>${clas.price}</td>
+                                <th>
+                                    <button className="btn bg-[#B2AB8C] text-white hover:text-orange-600 btn-xs">Pay</button>
+                                    <button onClick={() => deleteClass(clas._id)} className="btn bg-[#B2AB8C] text-white hover:text-orange-600 btn-xs">Delete</button>
+                                </th>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+
+
+                </table>
+            </div>
         </div>
     );
 };
